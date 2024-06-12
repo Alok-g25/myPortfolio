57a9dba33e568a80f248c062ed3809ccd21b0717
alok2001-kumar <alokkumar9506492158@gmail.com>
2024-06-12 20:33:49 +0530

    update file

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -17,16 +17,16 @@ function Project() {
                   data-aos-duration="2000"
                    className="my-5 p-4 card w-80 border-[2px] border-yellow-300 shadow-lg shadow-yellow-200">
                   <figure className="border-[1px] border-yellow-300 rounded-lg overflow-hidden">
-                    <img className="w-full h-48"
+                    <img className="w-full h-48 object-cover"
                       src={data.imageSrc}
-                      alt="Porject"
+                      alt="project"
                     />
                   </figure>
                   <div className="my-3">
-                    <h2 className="card-title">{data.title}</h2>
+                    <h2 className="card-title text-xl text-yellow-400">{data.title}</h2>
                     <p>{data.description}</p>
-                    <div className="card-actions justify-end">
-                      <a href={data.source}  className="btn btn-outline btn-warning">Code</a>
+                    <div className="card-actions justify-end mt-6">
+                      <a href={data.source}  className="px-2 py-1 border-[1px] border-yellow-300 btn-outline btn-warning rounded-lg duration-300">Code</a>
                     </div>
                   </div>
                 </div>
